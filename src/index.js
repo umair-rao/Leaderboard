7a0ab42439efaea79f43429ed603f36042614318
umair-rao <umairmuqeem@gmail.com>
2022-11-25 22:52:47 +0500

    Api Hit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import './style.css';
+import displayScores from './scoresList.js';
+
+const refresh = document.querySelector('#refresh-btn');
+const form = document.querySelector('form');
+
+const getScores = async () => {
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sr5jjs9jvWKzJUYcqGWW/scores',
+    { method: 'GET' },
+  )
+
+    .then((res) => res.json())
+    .then((data) => {
+      const list = data.result;
+      displayScores(list);
+    });
+};
+
+const saveList = async (entry) => {
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sr5jjs9jvWKzJUYcqGWW/scores/',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(entry),
+    },
+  );
+};
+
+class Score {
+  constructor(name, score) {
+    this.user = name;
+    this.score = score;
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#name').value;
+  const newScore = document.querySelector('#score').value;
+  const newEntry = new Score(name, newScore);
+  saveList(newEntry);
+  form.reset();
+});
+
+refresh.addEventListener('click', () => {
+  getScores();
+});
---
+++ src/scoresList.js
@@ -0,0 +1,14 @@
+const listContainer = document.querySelector('#socres');
+
+const displayScores = (list) => {
+  listContainer.innerHTML = '';
+  const newScoreList = list.map((item) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${item.user}: ${item.score}`;
+    return listItem;
+  });
+  newScoreList.forEach((element) => {
+    listContainer.appendChild(element);
+  });
+};
+export default displayScores;
